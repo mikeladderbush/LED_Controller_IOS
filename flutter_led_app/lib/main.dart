e6d466145413a8eaf9bf47395463c7469d4ce64f
Michael Ladderbush <mikeladderbush@gmail.com>
2025-04-07 12:54:18 -0400

    Switching to Dart/Flutter

---
+++ flutter_led_app/lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scoreboard Connection App',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Scoreboard Connection'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var client = http.Client();
+  Future<void> _activateScoreboard() async {
+    try {
+      var response = await client.post(Uri.https('scoreboard.com', 'path/path'));
+      var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+      var uri = Uri.parse(decodedResponse['uri'] as String);
+      print(await client.get(uri));
+    } finally {
+      client.close();
+    }
+    setState(() {
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Hello There'),
+            Text(
+              '$_activateScoreboard()',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _activateScoreboard,
+        tooltip: 'Turn On Scoreboard',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
